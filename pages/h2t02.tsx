d8e62ec223e9717a5cd451856338a9ecaee81e32
rolda2 <70531157+rolda2@users.noreply.github.com>
2024-02-29 15:12:42 +0100

    Update

--- pages/h2t02.tsx
+++ pages/h2t02.tsx
@@ -6,7 +6,7 @@ const Navbar: React.FC = () => {
         <nav className="flex justify-between mr-[170px] font-inter ml-[170px] bg-[#E6E6E6] text-[#000] text-2xl p-[48px_32px_48px_32px] mt-[92px]">
             <div className="">
                 <a href="/" className="">Home</a>
-                <a href="/contact" className="ml-[64px] mr-[64px]">Contact</a>
+                <a href="/contact" className="ml-[64px] mr-[64px]">Contacts</a>
                 <a href="/categories" className="">Categories</a>
             </div>
             <div>
