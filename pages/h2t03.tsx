f2883a143dd9d21f91e6b82e7da0e4411e539fa2
rolda2 <70531157+rolda2@users.noreply.github.com>
2024-03-07 14:54:41 +0100

    Update

---
+++ pages/h2t03.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '@/app/globals.css';
+
+const ColorShowerPage: React.FC = () => {
+    return (
+        <div className="flex flex-col items-center min-h-screen p-[256px] gap-[64px]">
+            <h1 className="text-4xl">Color Shower</h1>
+            <div className="flex justify-between items-center self-stretch">
+                <input type="text" className="flex flex-col justify-center items-start gap-[10px] self-stretch border-[1px] border-[#000] w-[384px]" />
+                <button className="flex justify-center items-center gap-[10px] bg-[#8eff7b] text-[#000] p-6 rounded-[10px]">Show</button>
+            </div>
+            <div className="w-24 h-24 bg-[#ffb200]"></div>
+            <div className="flex gap-[48px]">
+                <div className="w-16 h-16 bg-[#F00]"></div>
+                <div className="w-16 h-16 bg-[#d9d9d9]"></div>
+                <div className="w-16 h-16 bg-[#00A3FF]"></div>
+            </div>
+        </div>
+    );
+};
+
+export default ColorShowerPage;
